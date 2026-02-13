b61c86cddb1a01419140fd50652506602ab1a211
bernardsayegh <77056610+bernardsayegh@users.noreply.github.com>
2026-02-13 21:57:11 +1100

    Create desy3_likelihood.py

---
+++ desy3_likelihood.py
@@ -0,0 +1,131 @@
+"""
+DES Y3 Cosmic Shear Likelihood for Cobaya
+With Intrinsic Alignment (NLA model)
+"""
+
+import numpy as np
+from scipy import integrate
+from cobaya.likelihood import Likelihood
+import pyccl as ccl
+from astropy.io import fits
+import os
+
+class DESY3(Likelihood):
+    """DES Y3 cosmic shear likelihood with IA"""
+    
+    data_path: str = ""
+    
+    def initialize(self):
+        """Load data, covariance, and n(z)"""
+        
+        fits_file = os.path.join(self.data_path, 
+            "2pt_NG_final_2ptunblind_02_24_21_wnz_covupdate.v2.fits")
+        
+        with fits.open(fits_file) as f:
+            xip_data = f['xip'].data
+            xim_data = f['xim'].data
+            
+            self.xip_values = np.array(xip_data['VALUE'])
+            self.xim_values = np.array(xim_data['VALUE'])
+            self.xip_angles = np.array(xip_data['ANG'])
+            self.xim_angles = np.array(xim_data['ANG'])
+            self.xip_bin1 = np.array(xip_data['BIN1'])
+            self.xip_bin2 = np.array(xip_data['BIN2'])
+            self.xim_bin1 = np.array(xim_data['BIN1'])
+            self.xim_bin2 = np.array(xim_data['BIN2'])
+            
+            nz_source = f['nz_source'].data
+            self.z_nz = np.array(nz_source['Z_MID'])
+            self.nz = np.array([nz_source[f'BIN{i}'] for i in range(1, 5)])
+            
+            full_cov = np.array(f['COVMAT'].data)
+            
+        self.nzbins = 4
+        self.ndata_xip = len(self.xip_values)
+        self.ndata_xim = len(self.xim_values)
+        
+        self.data_vector = np.concatenate([self.xip_values, self.xim_values])
+        self.ndata = len(self.data_vector)
+        
+        # Extract covariance for xi+/xi-
+        self.cov = full_cov[:self.ndata, :self.ndata]
+        self.invcov = np.linalg.inv(self.cov)
+        
+        # Ell range for C_ell computation
+        self.ell = np.geomspace(2, 5000, 100)
+        
+        self.log.info(f"Loaded DES Y3: {self.ndata} data points")
+    
+    def get_requirements(self):
+        return {
+            'sigma8': None, 
+            'Omega_m': None, 
+            'omega_b': None, 
+            'H0': None, 
+            'n_s': None,
+            'A_IA': None,
+            'alpha_IA': None
+        }
+    
+    def logp(self, **params_values):
+        try:
+            sigma8 = self.provider.get_param('sigma8')
+            Omega_m = self.provider.get_param('Omega_m')
+            omega_b = self.provider.get_param('omega_b')
+            H0 = self.provider.get_param('H0')
+            n_s = self.provider.get_param('n_s')
+            A_IA = self.provider.get_param('A_IA')
+            alpha_IA = self.provider.get_param('alpha_IA')
+            
+            h = H0 / 100.0
+            Omega_b = omega_b / h**2
+            Omega_c = Omega_m - Omega_b
+            
+            cosmo = ccl.Cosmology(Omega_c=Omega_c, Omega_b=Omega_b, h=h,
+                                  n_s=n_s, sigma8=sigma8, transfer_function='bbks')
+            
+            tracers = []
+            for i in range(self.nzbins):
+                nz_norm = self.nz[i] / integrate.trapezoid(self.nz[i], self.z_nz)
+                
+                # NLA intrinsic alignment model: A_IA * ((1+z)/1.62)^alpha_IA
+                bias_ia = (self.z_nz, A_IA * ((1 + self.z_nz) / 1.62)**alpha_IA)
+                
+                tracer = ccl.WeakLensingTracer(
+                    cosmo, 
+                    dndz=(self.z_nz, nz_norm),
+                    has_shear=True,
+                    ia_bias=bias_ia
+                )
+                tracers.append(tracer)
+            
+            # Compute theory for xi+ and xi-
+            theory_xip = []
+            for i in range(self.ndata_xip):
+                b1, b2 = self.xip_bin1[i] - 1, self.xip_bin2[i] - 1
+                theta = self.xip_angles[i] / 60.0
+                cl = ccl.angular_cl(cosmo, tracers[b1], tracers[b2], self.ell)
+                xi = ccl.correlation(cosmo, ell=self.ell, C_ell=cl, 
+                                    theta=np.array([theta]), type='GG+', method='fftlog')
+                theory_xip.append(xi[0])
+            
+            theory_xim = []
+            for i in range(self.ndata_xim):
+                b1, b2 = self.xim_bin1[i] - 1, self.xim_bin2[i] - 1
+                theta = self.xim_angles[i] / 60.0
+                cl = ccl.angular_cl(cosmo, tracers[b1], tracers[b2], self.ell)
+                xi = ccl.correlation(cosmo, ell=self.ell, C_ell=cl, 
+                                    theta=np.array([theta]), type='GG-', method='fftlog')
+                theory_xim.append(xi[0])
+            
+            theory_vector = np.concatenate([theory_xip, theory_xim])
+            
+            # Chi-squared
+            diff = self.data_vector - theory_vector
+            chi2 = diff @ self.invcov @ diff
+            
+            return -0.5 * chi2
+            
+        except Exception as e:
+            self.log.debug(f"DES Y3 likelihood failed: {e}")
+            return -np.inf
